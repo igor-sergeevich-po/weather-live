e8e3b23153cce497085718f69bee0a3883fa3e8b
Igor_PO <89515076779@ya.ru>
2022-10-30 00:27:03 +0300

    init: add style header

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,12 @@
 import { getWeatherData } from "./api.js"
+import { createHeader } from "./appHeader.js";
 
 const app = async () => {
     const weather = await getWeatherData('Батайск');
+    const header = createHeader(weather.name)
+
+    document.body.append(header)
+
     console.log(weather)
 }
 
